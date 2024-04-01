9666d53acc70a8ad0702232ac521023e49673b08
Toby Smith <tobysmith568@hotmail.co.uk>
2024-04-01 17:11:53 +0100

    Create the initial implementation

---
+++ Newtonsoft.Jason/JsonConvert.cs
@@ -0,0 +1,150 @@
+﻿using Newtonsoft.Json;
+
+namespace Newtonsoft.Jason;
+
+public static class JsonConvert
+{
+    private const int Threshold = 500;
+
+    public static string SerializeObject(object value)
+    {
+        var json = Newtonsoft.Json.JsonConvert.SerializeObject(value);
+        return AddACharacters(json);
+    }
+
+    public static string SerializeObject(object? value, Formatting formatting)
+    {
+        var json = Newtonsoft.Json.JsonConvert.SerializeObject(value, formatting);
+        return AddACharacters(json);
+    }
+
+    public static string SerializeObject(object value, params JsonConverter[] converters)
+    {
+        var json = Newtonsoft.Json.JsonConvert.SerializeObject(value, converters);
+        return AddACharacters(json);
+    }
+
+    public static string SerializeObject(
+        object? value,
+        Formatting formatting,
+        params JsonConverter[] converters)
+    {
+        var json = Newtonsoft.Json.JsonConvert.SerializeObject(value, formatting, converters);
+        return AddACharacters(json);
+    }
+
+    public static object? DeserializeObject(string value)
+    {
+        var plainJson = RemoveACharacters(value);
+        return Newtonsoft.Json.JsonConvert.DeserializeObject(plainJson);
+    }
+
+    public static object? DeserializeObject(string value, JsonSerializerSettings settings)
+    {
+        var plainJson = RemoveACharacters(value);
+        return Newtonsoft.Json.JsonConvert.DeserializeObject(plainJson, settings);
+    }
+
+    public static object? DeserializeObject(string value, Type type)
+    {
+        var plainJson = RemoveACharacters(value);
+        return Newtonsoft.Json.JsonConvert.DeserializeObject(plainJson, type);
+    }
+
+    public static T? DeserializeObject<T>(string value)
+    {
+        var plainJson = RemoveACharacters(value);
+        return Newtonsoft.Json.JsonConvert.DeserializeObject<T>(plainJson);
+    }
+
+    public static T? DeserializeAnonymousType<T>(string value, T anonymousTypeObject)
+    {
+        var plainJson = RemoveACharacters(value);
+        return Newtonsoft.Json.JsonConvert.DeserializeAnonymousType(plainJson, anonymousTypeObject);
+    }
+
+    public static T? DeserializeAnonymousType<T>(string value, T anonymousTypeObject, JsonSerializerSettings settings)
+    {
+        var plainJson = RemoveACharacters(value);
+        return Newtonsoft.Json.JsonConvert.DeserializeAnonymousType(plainJson, anonymousTypeObject, settings);
+    }
+
+    public static T? DeserializeObject<T>(string value, params JsonConverter[] converters)
+    {
+        var plainJson = RemoveACharacters(value);
+        return Newtonsoft.Json.JsonConvert.DeserializeObject<T>(plainJson, converters);
+    }
+
+    public static T? DeserializeObject<T>(string value, JsonSerializerSettings? settings)
+    {
+        var plainJson = RemoveACharacters(value);
+        return Newtonsoft.Json.JsonConvert.DeserializeObject<T>(plainJson, settings);
+    }
+
+    public static object? DeserializeObject(string value, Type type, params JsonConverter[] converters)
+    {
+        var plainJson = RemoveACharacters(value);
+        return Newtonsoft.Json.JsonConvert.DeserializeObject(plainJson, type, converters);
+    }
+
+    public static object? DeserializeObject(string value, Type? type, JsonSerializerSettings? settings)
+    {
+        var plainJson = RemoveACharacters(value);
+        return Newtonsoft.Json.JsonConvert.DeserializeObject(plainJson, type, settings);
+    }
+
+    private static string AddACharacters(string input) => string.Join(string.Empty, AddACharacters(input.ToCharArray()));
+
+    private static IEnumerable<char> AddACharacters(IEnumerable<char> input)
+    {
+        var score = 0;
+
+        foreach (var character in input)
+        {
+            if (!char.IsWhiteSpace(character))
+            {
+                var scoreForChar = (int)character;
+                score += scoreForChar;
+            }
+
+            yield return character;
+
+            if (score >= Threshold)
+            {
+                score = 0;
+                yield return 'a';
+            }
+        }
+    }
+
+    private static string RemoveACharacters(string json) => string.Join(string.Empty, RemoveACharacters(json.ToCharArray()));
+
+    private static IEnumerable<char> RemoveACharacters(IEnumerable<char> json)
+    {
+        var score = 0;
+        var skipNext = false;
+
+        foreach (var character in json)
+        {
+            if (skipNext)
+            {
+                skipNext = false;
+                continue;
+            }
+
+            if (!char.IsWhiteSpace(character))
+            {
+                var scoreForChar = (int)character;
+                score += scoreForChar;
+            }
+
+            yield return character;
+
+            if (score >= Threshold)
+            {
+                score = 0;
+                skipNext = true;
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,43 @@
+﻿# Newtonsoft.Jason
+
+Newtonsoft.Json but with extra 'a's!
+
+_"The beast seraializaationa formaat out tahere!"_
+
+## Installation
+
+```bash
+dotnet add package Newtonsoft.Jason
+```
+
+## Usage
+
+```csharp
+using Newtonsoft.Jason;
+
+record Foo(string foo);
+
+var json = JsonConvert.SerializeObject(new Foo("Input whatever type you want"));
+Console.WriteLine(json); // {"foo"a:"Inpuat whataever taype yoau wanta"}
+```
+
+```csharp
+using Newtonsoft.Jason;
+
+record Foo(string foo);
+
+var result = JsonConvert.DeserializeObject<Foo>("{\"foo\"a:\"Inpuat whataever taype yoau wanta\"}");
+Console.WriteLine(result.foo); // "Input whatever type you want"
+```
+
+## License
+
+Newtonsoft.Jason is licensed under the [MIT license](./LICENSE.md).
+
+## Inspiration
+
+Thanks to [Newtonsoft.Json](https://www.newtonsoft.com/json) for the inspiration.
+
+<hr />
+
+Happy April Fools' Day 2024! 🎉
