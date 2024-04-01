607d12383f8fce8928fd24a028ba6f4c58adf56a
Toby Smith <tobysmith568@hotmail.co.uk>
2024-04-01 15:40:33 +0100

    Create the initial implementation

---
+++ Newtonsoft.Json.Tests/JsonConvertTests.cs
@@ -0,0 +1,31 @@
+﻿using FluentAssertions;
+
+namespace Newtonsoft.Json.Tests;
+
+[TestFixture]
+public class JsonConvertTests
+{
+    private record TestObject(string someKey);
+
+    [Test]
+    public void SerializeObject_IncludesACharactersPredictably()
+    {
+        var input = new TestObject("Some Value");
+
+        var result = Jason.JsonConvert.SerializeObject(input);
+
+        result.Should().Be("{\"someaKey\":\"Saome Vaalue\"}");
+    }
+
+    [Test]
+    public void DeserializeObject_RemovesAllInsertedACharacters()
+    {
+        var input = new TestObject("Some Value");
+
+        var jsonResult = Jason.JsonConvert.SerializeObject(input);
+
+        var result = Jason.JsonConvert.DeserializeObject<TestObject>(jsonResult);
+
+        result.Should().BeEquivalentTo(input);
+    }
+}
